e3693027c00c74d2767424c830e539989a23689f
qsdcv301 <130839376+qsdcv301@users.noreply.github.com>
2024-09-06 12:44:33 +0900

    feat/stmp연결 작업 및 postPage 구현

--- js/post.sciprt.js
+++ js/post.sciprt.js
@@ -1,31 +1,45 @@
 $(document).ready(function () {
-  //마크다운 파일 목록 가져오기 getJSON
-  $.getJSON("posts/posts.list.json", function (data) {
-    const mkFiles = data.posts.map((post) => `posts/${post}`);
+  const postsPerPage = 5;
+  const pagesPerGroup = 10;
+  let currentPage = 1;
+  let totalPosts = 0;
+  let totalPages = 0;
 
-    //각각의 포스트를 가져와서 표시
-    $.each(mkFiles, function (index, file) {
-      $.get(file, function (text) {
-        const { meta, content } = parseMarkDown(text);
-        //console.log(meta);
-        //console.log(content);
+  function postsLoad(page) {
+    //마크다운 파일 목록 가져오기 getJSON
+    $.getJSON("posts/posts.list.json", function (data) {
+      totalPosts = data.posts.length;
+      totalPages = Math.ceil(totalPosts / postsPerPage);
+      const start = (page - 1) * postsPerPage;
+      const end = start + postsPerPage;
+      const postsToShow = data.posts.slice(start, end);
 
-        //메타데이터 저장 및 데이터가 없을 경우 기본값 설정
-        const title = meta.title || "Untitled Post";
-        const date = meta.date || "날짜미상";
-        const category = meta.category || "미분류";
-        const image = meta.image || "/blog/images/posts/default.jpg";
+      $("#myposts").empty();
 
-        //본문에서 HTML 코드 필터링
-        const snippet = marked
-          .parse(content.split("\n").slice(0, 6).join("\n"))
-          .replace(/<\/?[^>]+(>|$)/g, "");
-        const postLink = `article.html?file=${file}`;
+      //각각의 포스트를 가져와서 표시
+      $.each(postsToShow, function (index, file) {
+        file = `posts/${file}`;
+        $.get(file, function (text) {
+          const { meta, content } = parseMarkDown(text);
+          //console.log(meta);
+          //console.log(content);
 
-        //포스팅 쓰기
-        const newPost = index === 0; //첫 포스트는 크게
-        //html 만들기
-        const postHTML = `
+          //메타데이터 저장 및 데이터가 없을 경우 기본값 설정
+          const title = meta.title || "Untitled Post";
+          const date = meta.date || "날짜미상";
+          const category = meta.category || "미분류";
+          const image = meta.image || "/res/images/posts/default.jpg";
+
+          //본문에서 HTML 코드 필터링
+          const snippet = marked
+            .parse(content.split("\n").slice(0, 10).join("\n"))
+            .replace(/<\/?[^>]+(>|$)/g, "");
+          const postLink = `article.html?file=${file}`;
+
+          //포스팅 쓰기
+          const newPost = index === 0; //첫 포스트는 크게
+          //html 만들기
+          const postHTML = `
             <div class="${newPost ? "col-12" : "col-6"}">
                       <div class="bg-white m-3 p-5 shadow-sm  myposts">
                           <div class="img-box"> 
@@ -41,29 +55,89 @@ $(document).ready(function () {
                      </div>
                   </div>
             `;
-        //포스트를 id mypost에 append
-        $("#myposts").append(postHTML);
-      });
-    });
-
-    function parseMarkDown(text) {
-      const regex = /^---\s*([\s\S]+?)\s*---\s*([\s\S]*)$/;
-      const match = text.match(regex);
-
-      if (match) {
-        const meta = {};
-        match[1].split("\n").forEach((line) => {
-          const [key, value] = line.split(":").map((str) => str.trim());
-          if (key && value) {
-            //console.log(key, "-" , value);
-            meta[key] = value.replace(/['"]/g, ""); //따옴표 제거
-          }
+          //포스트를 id mypost에 append
+          $("#myposts").append(postHTML);
+        }).fail(function () {
+          console.error("데이터를 읽어오는데 에러가 발생했습니다.", file);
         });
-        const content = match[2];
-        return { meta, content };
-      } else {
-        return { meta: {}, content: text };
+      });
+
+      //페이징 함수
+      function renderPagination(totalPages, currentPage) {
+        const currentGroup = Math.ceil(currentPage / pagesPerGroup);
+        const startPage = (currentGroup - 1) * pagesPerGroup + 1;
+        const endPage = Math.min(startPage + pagesPerGroup - 1, totalPages);
+        let pageHtml = "";
+        //이전
+        if (currentGroup > 1) {
+          const pervGroupPage = (currentGroup - 1) * pagesPerGroup;
+          pageHtml += `
+              <li class="page-item">
+                  <a class="page-link" href="${pervGroupPage}">
+                      <i class="ri-arrow-left-s-line"></i>
+                  </a>
+              </li>
+              `;
+        }
+
+        for (let i = startPage; i <= endPage; i++) {
+          let active = currentPage == i ? "active" : "";
+          console.log(currentPage, active);
+          pageHtml += `
+              <li class="page-item ${active}">
+                  <a class="page-link" href="${i}">
+                      ${i}
+                  </a>
+              </li>
+              `;
+        }
+
+        //다음
+        if (currentGroup * pagesPerGroup < totalPages) {
+          const nextGroupPage = currentGroup * pagesPerGroup + 1;
+          pageHtml += `
+              <li class="page-item">
+                  <a class="page-link" href="${nextGroupPage}">
+                      <i class="ri-arrow-right-s-line"></i>
+                  </a>
+              </li>
+              `;
+        }
+
+        $(".pagination").html(pageHtml);
       }
+      renderPagination(totalPages, currentPage);
+    });
+  } //end posts
+
+  postsLoad(currentPage);
+
+  $(document).on("click", ".page-link", function (e) {
+    e.preventDefault();
+    const pg = $(this).attr("href");
+    if (pg !== currentPage) {
+      currentPage = pg;
+      postsLoad(currentPage);
     }
   });
-});
+}); //.jquery
+
+function parseMarkDown(text) {
+  const regex = /^---\s*([\s\S]+?)\s*---\s*([\s\S]*)$/;
+  const match = text.match(regex);
+
+  if (match) {
+    const meta = {};
+    match[1].split("\n").forEach((line) => {
+      const [key, value] = line.split(":").map((str) => str.trim());
+      if (key && value) {
+        //console.log(key, "-" , value);
+        meta[key] = value.replace(/['"]/g, ""); //따옴표 제거
+      }
+    });
+    const content = match[2];
+    return { meta, content };
+  } else {
+    return { meta: {}, content: text };
+  }
+}
